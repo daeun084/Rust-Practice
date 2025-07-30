3be9e8e66c01f9b562de3eaf3383263c7d01e7f2
daeun084 <030804jk@naver.com>
2025-07-30 21:58:44 +0900

    e14, e15, quiz3 : generic, traits

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -18,13 +18,12 @@ struct OtherStruct;
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
-// TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+// trait bound
+fn some_func<T: OtherTrait + SomeTrait>(item: T) -> bool {
     item.some_function() && item.other_function()
 }
 
 fn main() {
-    // You can optionally experiment here.
 }
 
 #[cfg(test)]
--- exercises/quizzes/quiz3.rs
+++ exercises/quizzes/quiz3.rs
@@ -1,25 +1,12 @@
-// This quiz tests:
-// - Generics
-// - Traits
-//
-// An imaginary magical school has a new report card generation system written
-// in Rust! Currently, the system only supports creating report cards where the
-// student's grade is represented numerically (e.g. 1.0 -> 5.5). However, the
-// school also issues alphabetical grades (A+ -> F-) and needs to be able to
-// print both types of report card!
-//
-// Make the necessary code changes in the struct `ReportCard` and the impl
-// block to support alphabetical report cards in addition to numerical ones.
+use std::fmt;
 
-// TODO: Adjust the struct as described above.
-struct ReportCard {
-    grade: f32,
+struct ReportCard<T> {
+    grade: T,
     student_name: String,
     student_age: u8,
 }
 
-// TODO: Adjust the impl block as described above.
-impl ReportCard {
+impl<T: fmt::Display> ReportCard<T> {
     fn print(&self) -> String {
         format!(
             "{} ({}) - achieved a grade of {}",
@@ -29,7 +16,6 @@ impl ReportCard {
 }
 
 fn main() {
-    // You can optionally experiment here.
 }
 
 #[cfg(test)]
